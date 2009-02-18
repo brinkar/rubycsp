3e2784e0d636295eb64524fcf253ffef56381438
Henrik Brink <henrikbrink@gmail.com>
2009-02-18 16:43:32 +0100

    Processes can now return values when run, and process lists return a list of values from the running of each of its processes

--- csp.rb
+++ csp.rb
@@ -1,5 +1,5 @@
 
-# TODO: Alternatative class
+# TODO: Alternative class
 # TODO: Guards?
 
 require "thread"
@@ -61,15 +61,16 @@ module CSP
 		end
 		
 		def run(args = [])
-			# TODO: Handle return values
+			retval = nil
 			begin 
-				@block.call(*args)
+				retval = @block.call(*args)
 			rescue Channel::Poison
 				args.each do |arg|
 					arg.poison if (arg.is_a?(Channel) or arg.is_a?(Channel::End)) and not arg.poisoned?
 				end
 				puts "#{@id}: Dying from poisoning..."
 			end
+			return retval
 		end
 	
 	end
@@ -82,26 +83,31 @@ module CSP
 			@processes = []
 		end
 			
-		def run(method)
-			# TODO: Handle return values
+		def run(method = :sequential)
+			retvals = []
 			case method
 			when :parallel
 				threads = []
 				@processes.each do |p|
-					threads << Thread.new { p[:process].run(p[:args]) }
+					threads << Thread.new do
+						retvals[@processes.index(p)] = p[:process].run(p[:args])
+					end
 				end
 				threads.each do |t|
 					t.join
 				end
 			when :sequential
 				@processes.each do |p|
-					p[:process].run p[:args]
+					retvals[@processes.index(p)] = p[:process].run p[:args]
 				end
 			end
+			return retvals
 		end
 	
 		def each(&block)
-			@processes.each block
+			@processes.each do |p|
+				yield p
+			end
 		end
 		
 		def add(process, *args)
